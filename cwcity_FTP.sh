a0beecc1017acdc1ed875c3221441e44ea699060
flexdigit <georg-org@hotmail.de>
2015-02-05 21:06:00 +0100

    Update cwcity_FTP.sh

--- cwcity_FTP.sh
+++ cwcity_FTP.sh
@@ -2,21 +2,24 @@
 
 # FTP
 FTP_SERVER=flexdigit.cwsurf.de
-FTP_USER=USER
-FTP_PW=PW
+FTP_USER=usr_ftp_216891
+FTP_PW=gebuit00_Zaka2
 
 #cd .
 
 #ftp -n -v $FTP_SERVER << EOT
 ftp -n $FTP_SERVER << EOT
 user $FTP_USER $FTP_PW
-cd /htdocs
+
 bin
 prompt
+
+cd /htdocs
 mput tempday.png
 mput solarday.png
 mput tempweek.png
 mput solarweek.png
+
 by
 EOT
 
